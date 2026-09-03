f97806d4ecf9066e634851422f81f11f167e528e
carrombro47-crypto <carrombro47@gmail.com>
2026-09-03 10:45:19 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -7,7 +7,7 @@ _client = None
 # by setting a MONGO_URI environment variable on Render (recommended),
 # without touching this file.
 _DEFAULT_MONGO_URI = (
-    "mongodb+srv://developerbro723_db_user:9axC7c7iQm0G3ESO@cluster0.dr8m75m.mongodb.net/?appName=Cluster0"
+    "mongodb+srv://carrombro47_db_user:St7FJBRs0pPYYmt3@cluster0.fp3wrat.mongodb.net/?appName=Cluster0"
 )
 
 
